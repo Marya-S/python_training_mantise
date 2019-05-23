52e18078faa53fe934818737bd80c6dbf99bebc2
Mary <7333581>
2019-05-23 11:57:10 +0300

    Исправлены проверки

--- test/DeleteProject.py
+++ test/DeleteProject.py
@@ -6,11 +6,20 @@ def test_delete_project(app):
     if app.project.get_project_list() == 0:
         app.project.add_new_project(
             Project(projectname="NewDelTest", description="Я создан для удаления"))
-    old_projects = app.soap.get_project_list_soap("administrator", "root")
+    old_projects = change_type(app.soap.get_project_list_soap())
     app.project.delete(old_projects)
     time.sleep(3)
-    new_projects = app.soap.get_project_list_soap("administrator", "root")
+    new_projects = change_type(app.soap.get_project_list_soap())
     assert len(old_projects) - 1 == len(new_projects)
     old_projects[0:1] = []
-    project = new_projects[1].name
     assert sorted(old_projects, key=Project.id_or_max) == sorted(new_projects, key=Project.id_or_max)
+
+def change_type(DataProject):
+    project_list=[]
+    for item in DataProject:
+       name = item.name
+       description = item.description
+       id = item.id
+       project = Project(projectname=name, description=description, id=id)
+       project_list.append(project)
+    return project_list
